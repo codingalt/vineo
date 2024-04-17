0e0ec8e568652368140ee14495f44ea003ce60e7
Coding Alt <codingalt7@gmail.com>
2024-04-16 22:52:46 -0700

    fixes

--- src/components/Onboarding/OnboardingSwiper.jsx
+++ src/components/Onboarding/OnboardingSwiper.jsx
@@ -1,120 +1,144 @@
-// import React, { useState } from "react";
-// import css from "./Onboarding.module.scss";
-// import logo from "../../assets/logo.png";
-// import EnterName from "./EnterName";
-// import UploadPicture from "./UploadPicture";
-// import SetRate from "./SetRate";
-// import playBtn from "../../assets/play-button.png";
-// import { useTheme } from "@mui/material/styles";
-// import SwipeableViews from "react-swipeable-views-react-18-fix";
+import React, { useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
+import { wrap } from "popmotion";
+import css from "./Onboarding.module.scss";
+import logo from "../../assets/logo.png";
+import EnterName from "./EnterName";
+import UploadPicture from "./UploadPicture";
+import SetRate from "./SetRate";
+import playBtn from "../../assets/play-button.png";
+import GetStarted from "./GetStarted";
 
-// const OnboardingSwiper = () => {
-//   const theme = useTheme();
-//   const [activeStep, setActiveStep] = useState(0);
-//   const maxSteps = 4;
+const variants = {
+  enter: (direction) => {
+    return {
+      x: direction > 0 ? 1000 : -1000,
+      opacity: 0,
+      position: "absolute",
+    };
+  },
+  center: {
+    zIndex: 1,
+    x: 0,
+    opacity: 1,
+    position: "relative",
+  },
+  exit: (direction) => {
+    return {
+      zIndex: 0,
+      x: direction < 0 ? 1000 : -1000,
+      opacity: 0,
+      position: "absolute",
+    };
+  },
+};
 
-//   const handleNext = () => {
-//     setActiveStep((prevActiveStep) => prevActiveStep + 1);
-//   };
+const swipeConfidenceThreshold = 10000;
+const swipePower = (offset, velocity) => {
+  return Math.abs(offset) * velocity;
+};
 
-//   const handleBack = () => {
-//     setActiveStep((prevActiveStep) => prevActiveStep - 1);
-//   };
-
-//   const handleStepChange = (step) => {
-//     setActiveStep(step);
-//   };
-
-//   return (
-    // <div className="w-screen h-screen max-w-sm overflow-hidden flex justify-center items-center flex-col">
-    //   <div className={css.containerOnBoarding}>
-    //     <SwipeableViews
-    //       axis={theme.direction === "rtl" ? "x-reverse" : "x"}
-    //       index={activeStep}
-    //       onChangeIndex={handleStepChange}
-    //       enableMouseEvents
-    //     >
-    //       {/* First Screen  */}
-    //       <div className={css.wrapper}>
-    //         <div className={css.logo}>
-    //           <img src={logo} alt="" />
-    //         </div>
-
-    //         <div className={css.content}>
-    //           <p>Set Up Your Profile</p>
-    //           <span>
-    //             Personalize your experience by creating a profile. Share your
-    //             interests, add a photo, and let others get to know the real you.
-    //           </span>
-    //         </div>
-    //       </div>
-
-    //       {/* Second Screen  */}
-    //       <EnterName />
-
-    //       {/* Third Screen  */}
-    //       <UploadPicture />
-
-    //       {/* Fourth Screen  */}
-    //       <SetRate />
-    //     </SwipeableViews>
-
-    //     <div className="flex w-full space-x-2 items-center justify-center mx-auto mb-5 mt-36">
-    //       {Array(4)
-    //         .fill(null)
-    //         .map((item, index) =>
-    //           activeStep === index ? (
-    //             <div
-    //               key={index}
-    //               className="w-[20px] h-[8px] bg-[#3632FF] rounded-full cursor-pointer transition-all"
-    //             ></div>
-    //           ) : (
-    //             <div
-    //               key={index}
-    //               onClick={() => handleStepChange(index)}
-    //               className="w-[7px] h-[8px] bg-white rounded-full cursor-pointer transition-all"
-    //             ></div>
-    //           )
-    //         )}
-    //     </div>
-
-    //     {activeStep === 0 && (
-    //       <button
-    //         onClick={handleNext}
-    //         disabled={activeStep === maxSteps - 1}
-    //         className={css.getStartedBtn}
-    //       >
-    //         Get Started
-    //       </button>
-    //     )}
-
-    //     {(activeStep === 1 || activeStep === 2) && (
-    //       <div className={css.skipNextBtns}>
-    //         <button onClick={handleNext} className={css.skip}>
-    //           Skip
-    //         </button>
-    //         <button onClick={handleNext} className={css.next}>
-    //           <img src={playBtn} alt="" />
-    //         </button>
-    //       </div>
-    //     )}
-
-    //     {activeStep === 3 && (
-    //       <button className={css.getStartedBtn}>Finsih</button>
-    //     )}
-    //   </div>
-    // </div>
-//   );
-// };
-
-// export default OnboardingSwiper;
-
-import React from 'react'
+const renderData = [
+  <GetStarted />,
+  <EnterName />,
+  <UploadPicture />,
+  <SetRate />,
+];
 
 const OnboardingSwiper = () => {
-  return (
-    <div>OnboardingSwiper</div>
-  )
-}
+  const [[page, direction], setPage] = useState([0, 0]);
 
-export default OnboardingSwiper
+  const dataIndex = wrap(0, renderData.length, page);
+
+  const paginate = (newDirection) => {
+    const nextPage = page + newDirection;
+    if (nextPage >= 0 && nextPage <= 3) {
+      setPage([nextPage, newDirection]);
+    }
+  };
+
+  return (
+    <>
+      <div className="w-screen h-screen max-w-sm overflow-hidden flex justify-center items-center flex-col mx-auto">
+        <div className={css.containerOnBoarding}>
+          <AnimatePresence initial={false} custom={direction}>
+            <motion.div
+              key={page}
+              custom={direction}
+              variants={variants}
+              initial="enter"
+              animate="center"
+              exit="exit"
+              transition={{
+                x: { type: "spring", stiffness: 300, damping: 30 },
+                opacity: { duration: 0.2 },
+              }}
+              drag="x"
+              dragConstraints={{ left: 0, right: 0 }}
+              dragElastic={1}
+              onDragEnd={(e, { offset, velocity }) => {
+                const swipe = swipePower(offset.x, velocity.x);
+
+                if (swipe < -swipeConfidenceThreshold) {
+                  paginate(1);
+                } else if (swipe > swipeConfidenceThreshold) {
+                  paginate(-1);
+                }
+              }}
+            >
+              {renderData[dataIndex]}
+            </motion.div>
+          </AnimatePresence>
+
+          <div className="flex w-full absolute left-0 right-0 bottom-48 space-x-2 items-center justify-center mx-auto mb-5 mt-36">
+            {Array(4)
+              .fill(null)
+              .map((item, index) =>
+                page === index ? (
+                  <div
+                    key={index}
+                    className="w-[20px] h-[8px] bg-[#3632FF] rounded-full cursor-pointer transition-all"
+                  ></div>
+                ) : (
+                  <div
+                    key={index}
+                    onClick={() => paginate(index - page)}
+                    className="w-[7px] h-[8px] bg-white rounded-full cursor-pointer transition-all"
+                  ></div>
+                )
+              )}
+          </div>
+
+          {page === 0 && (
+            <button
+              onClick={() => paginate(1)}
+              disabled={page === 4 - 1}
+              className={css.getStartedBtn}
+            >
+              Get Started
+            </button>
+          )}
+
+          {(page === 1 || page === 2) && (
+            <div className={css.skipNextBtns}>
+              <button onClick={() => paginate(1)} className={css.skip}>
+                Skip
+              </button>
+              <button onClick={() => paginate(1)} className={css.next}>
+                <img src={playBtn} alt="" />
+              </button>
+            </div>
+          )}
+
+          {page === 3 && (
+            <button disabled={page === 3} className={css.getStartedBtn}>
+              Finsih
+            </button>
+          )}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default OnboardingSwiper;
--- src/pages/OnboardingPage.jsx
+++ src/pages/OnboardingPage.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
 import OnboardingSwiper from '../components/Onboarding/OnboardingSwiper';
-import Test from '../components/Onboarding/Test';
 
 const OnboardingPage = () => {
   return (
-    // <OnboardingSwiper />
-    <Test />
+    <OnboardingSwiper />
   );
 }
 
